55d1ea81e5e233900b43b04caedd9b5c8af04d2b
Irfan Alwari <m.irfan_ahmed@hotmail.com>
2020-04-12 07:09:43 +0500

    analytics google

--- index.php
+++ index.php
@@ -19,5 +19,15 @@
         echo $row->user_name."<br>";
     }
     ?>
+    
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-129668580-1"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-129668580-1');
+</script>
 </body>
 </html>
